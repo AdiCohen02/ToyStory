50aa3fa2654b321115041af0dee5f13d56c19dcf
97258 <libby.chvalevsky@mail.huji.ac.il>
2022-03-29 21:05:53 +0300

    "helper added"

--- app/src/main/java/com/example/myapplication/helper.java
+++ app/src/main/java/com/example/myapplication/helper.java
@@ -3,33 +3,44 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class helper extends AppCompatActivity {
-
-    private EditText name;
-    private Button start_recording, stop_recording, play_recording, submit_recording;
+    ImageButton playRecord;
+    private Button getHomeBtn;
+    private Button gameBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.helper);
 
-        name = findViewById(R.id.name);
-        start_recording = findViewById(R.id.start_recording);
-        stop_recording = findViewById(R.id.stop_recording);
-        play_recording = findViewById(R.id.play_recording);
-        submit_recording = findViewById(R.id.submit_recording);
+         playRecord = findViewById(R.id.playRecord);
+         gameBtn=findViewById(R.id.gameBtn);
+         getHomeBtn=findViewById(R.id.getBackHomeBtn_inHelper);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.bark);
+        playRecord.setOnClickListener(new View.OnClickListener(){
 
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
 
-
-        submit_recording.setOnClickListener(new View.OnClickListener() {
+        getHomeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(helper.this, settingsPage.class);
+            public void onClick (View v){
+                startActivity(new Intent(helper.this, homePage.class));
+            }
+        });
+        gameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                startActivity(new Intent(helper.this, gamePage.class));
             }
         });
     }
